d818a3601184f81f35bb382b3a2ee18dd6468219
Edward Wang <94341993+whatsup-world@users.noreply.github.com>
2022-06-15 17:23:36 -0700

    initial commit

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,38 +1,55 @@
-import React from 'react';
+import React from "react";
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
-import './ProductView.css'
+import "./ProductView.css";
+import { useState, useEffect } from "react";
 
 function ProductView({ products }) {
+  // TODO: Replace with state variable
+  const [sideOpen, setSideOpen] = useState(true);
+  const [selectedProduct, setSelectedProduct] = useState("");
 
-    // TODO: Replace with state variable
-    const sideOpen = true;
+  useEffect(() => {
+    if (selectedProduct) {
+      setSideOpen(true);
+    }
+  }, [selectedProduct]);
 
-    return (
-        <div className="product-view">
-            <div className="product-main-area">
-                <h1>Products</h1>
-                <div className="product-list">
-                    {products.map(item =>
-                        <ProductListItem
-                            key={item.id}
-                            product={item}
-                            onClick={() => console.log('SELECT PRODUCT', item)}
-                        />
-                    )}
-                </div>
-            </div>
-            <div className="product-side-panel">
-                <div className="product-side-panel-toggle-wrapper">
-                    <div className="product-side-panel-toggle"
-                         onClick={() => console.log('TOGGLE SIDE PANEL')}>
-                        {sideOpen ? '>' : '<'}
-                    </div>
-                </div>
-                <ProductDetails visible={sideOpen} />
-            </div>
+  useEffect(() => {
+    if (!sideOpen) {
+      setSelectedProduct();
+    }
+  }, [sideOpen]);
+
+  return (
+    <div className="product-view">
+      <div className="product-main-area">
+        <h1>Products</h1>
+        <div className="product-list">
+          {products.map((item) => (
+            <ProductListItem
+              key={item.id}
+              product={item}
+              isSelected={selectedProduct && item.id === selectedProduct.id}
+              onClick={(e) => setSelectedProduct(item)}
+            />
+          ))}
         </div>
-    );
+      </div>
+      <div className="product-side-panel">
+        <div className="product-side-panel-toggle-wrapper">
+          <div
+            className="product-side-panel-toggle"
+            onClick={() => setSideOpen(!sideOpen)}
+          >
+            {sideOpen ? ">" : "<"}
+          </div>
+        </div>
+
+        <ProductDetails product={selectedProduct} visible={sideOpen} />
+      </div>
+    </div>
+  );
 }
 
 export default ProductView;
